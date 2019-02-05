24ed208a475635d5e61c77e5773b3edede73cf97
yishai <yishai5555@gmail.com>
2019-02-05 10:27:49 +0200

    b4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,43 @@ import { Component } from '@angular/core';
     </div>
     <h2 style="color:blue">primeng Train </h2>
      
-     
+    <p-accordion>
+    <p-accordionTab header="Salads">
+      Salads...
+    </p-accordionTab>
+    <p-accordionTab header="Pasta">
+      Pasta...
+    </p-accordionTab>
+    <p-accordionTab header="Pizza" [selected]="true">
+      <p-radioButton
+        label="Double cheese 🧀🧀"
+        name="pizza"
+        value="double-cheese"
+        [(ngModel)]="pizzaSelection">
+      </p-radioButton><br>
+  
+      <p-radioButton
+        label="Anchovy"
+        name="pizza"
+        value="anchovy"
+        [(ngModel)]="pizzaSelection">
+      </p-radioButton><br>
+  
+      <p-radioButton
+        label="Meat lover's 🍖"
+        name="pizza"
+        value="meat-lover"
+        [(ngModel)]="pizzaSelection">
+      </p-radioButton>
+    </p-accordionTab>
+  </p-accordion>
+  
+  <br>
+  
+  <p-panel header="Extras" *ngIf="pizzaSelection && pizzaSelection.length">
+    🍕 Would you like extra cheese on your pizza?
+    <button pButton type="button" label="Ok, yeah!"></button>
+  </p-panel>
     <router-outlet></router-outlet>
   `,
   styles: []
